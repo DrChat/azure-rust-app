a5c817720245acdcde7f244a537653958010c5d4
Justin Moore <jusmoore@microsoft.com>
2023-02-17 12:54:32 -0600

    Basic Azure app example

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+#![feature(decl_macro)]
+#[macro_use]
+extern crate rocket;
+
+use rocket::{
+    form::{Form, FromForm, FromFormField},
+    fs::FileServer,
+};
+use rocket_dyn_templates::{context, Template};
+
+#[cfg(test)]
+mod tests;
+
+#[derive(Debug, FromForm)]
+#[allow(dead_code)]
+struct Submit<'v> {
+    #[field(validate = len(1..))]
+    name: &'v str,
+}
+
+#[post("/hello", data = "<form>")]
+fn hello(form: Form<Submit<'_>>) -> Template {
+    Template::render(
+        "hello",
+        context! {
+            name: form.name,
+        },
+    )
+}
+
+#[get("/")]
+pub fn index() -> Template {
+    Template::render("index", context! {})
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/static", FileServer::from("static"))
+        .mount("/", routes![index, hello])
+        .attach(Template::fairing())
+}
